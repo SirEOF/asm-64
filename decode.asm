7ab80f5bca6b1393b8f8372c62076723bbbd91fd
Mharcos Nesster <mh4root@gmail.com>
2014-08-04 11:05:04 -0300

    Create decode.asm

---
+++ decode.asm
@@ -0,0 +1,14 @@
+BITS 32
+jmp short jmptrick
+decoder:
+pop esi
+xor ecx,ecx
+mov cl,0
+loop:
+sub byte [esi+ecx-1],0
+dec cl
+jnz loop
+jmp short obfuscated_code
+jmptrick:
+call decoder
+obfuscated_code:
